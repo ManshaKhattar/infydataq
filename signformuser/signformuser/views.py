4bb719370f89df144670722f41d7b9fa82b702d9
PrathyushaGottipati <101118953+PrathyushaGottipati@users.noreply.github.com>
2022-03-24 20:07:33 +0530

    Update views.py

--- signformuser/signformuser/views.py
+++ signformuser/signformuser/views.py
@@ -5,10 +5,20 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.db import IntegrityError
 from django.contrib.auth import login, logout
+import re
 
 def signnewuser(request):
     if request.method=="POST":
-        if request.POST.get('password1')==request.POST.get('password2'):
+        if request.POST.get('username')[0].islower():
+            return render(request,'Signup.html',{'form':UserCreationForm(),'error': 'The Username should start with uppercase letter...'})
+        if int(request.POST.get('age'))<20:
+            return render(request,'Signup.html',{'form':UserCreationForm(),'error': 'The age must be greater than 20...'})
+        regex = r'\b[A-Za-z._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if( not (re.fullmatch(regex, request.POST.get('email')))):
+            return render(request,'Signup.html',{'form':UserCreationForm(),'error': 'please enter valid Email...'})
+        if len(request.POST.get('password1'))<8:
+            return render(request,'Signup.html',{'form':UserCreationForm(),'error': 'The Password length should be minimum 8 characters...'})
+        elif request.POST.get('password1')==request.POST.get('password2'):
           try:
             saveuser=User.objects.create_user(request.POST.get('username'),password=request.POST.get('password1'))
             saveuser.save()
@@ -35,7 +45,7 @@ def loginuser(request):
         return render(request,'Login.html',{'form':AuthenticationForm()})  
 
 def home(request): 
-     return render(request,'home.html')    
+    return render(request,'Page1.html')    
 
 def Welcomepage(request):
     return render(request,'welcome.html')       
@@ -44,3 +54,9 @@ def logoutpage(request):
     if request.method=="POST":
         logout(request)
         return redirect('loginuser')    
+def adminlogin(request):
+    return render(request,'ADMINLOGIN.html')
+def adminsignup(request):
+    return render(request,'ADMINSIGNUP.html')
+def adminwelcome(request):
+    return render(request,'ADMINWELCOME.html')
